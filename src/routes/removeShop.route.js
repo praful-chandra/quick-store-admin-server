fc31f57c4a51d84f1a40b9a4d143c5da96cf9d23
praful chandra <praful.chandra@yahoo.com>
2020-06-01 17:04:09 +0530

    Added delete for coupons,sales,campaigns

--- src/routes/removeShop.route.js
+++ src/routes/removeShop.route.js
@@ -6,6 +6,9 @@ const AuthmiddleWare = require("../middleware/authMiddleWare");
 
 const Product = mongoose.model("product");
 const Category = mongoose.model("category");
+const Campaign = mongoose.model("campaign");
+const Sale = mongoose.model("sale");
+const Coupon = mongoose.model("coupon");
 
 router.delete("/deleteProduct", AuthmiddleWare, async (req, res) => {
   const PId = req.body._id;
@@ -38,5 +41,62 @@ router.delete("/deleteCategory", AuthmiddleWare, async (req, res) => {
     res.status(500).json("error");
   }
 });
+
+router.delete("/deleteCampaign",AuthmiddleWare,async (req,res)=>{
+  const CampId = req.body._id;
+
+  try{
+
+    if(!CampId) throw Error();
+
+    const campaign = await Campaign.findById(CampId);
+    campaign.remove().then(()=>{
+      res.json({success : true});
+    })
+
+  }
+  catch (err){   
+    res.status(500).json("Error");
+  }
+
+})
+
+router.delete("/deleteSale",AuthmiddleWare,async (req,res)=>{
+  const saleId = req.body._id;
+
+  try{
+
+    if(!saleId) throw Error();
+
+    const sale = await Sale.findById(saleId);
+    sale.remove().then(()=>{
+      res.json({success : true});
+    })
+
+  }
+  catch (err){   
+    res.status(500).json("Error");
+  }
+
+})
+
+router.delete("/deletecoupon",AuthmiddleWare,async (req,res)=>{
+  const coupId = req.body._id;
+
+  try{
+
+    if(!coupId) throw Error();
+
+    const coupon = await Coupon.findById(coupId);
+    coupon.remove().then(()=>{
+      res.json({success : true});
+    })
+
+  }
+  catch (err){   
+    res.status(500).json("Error");
+  }
+
+})
 
 module.exports = router;
